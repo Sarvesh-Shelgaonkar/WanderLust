5853f7e971375662bc5a93641291774313f5a3ee
Sarvesh-Shelgaonkar <sarveshshelgaonkar864@gmail.com>
2025-01-08 11:31:05 +0530

    Databse Initialization

--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-const listingSchema = new Schema({
+const listingSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
@@ -9,6 +8,8 @@ const listingSchema = new Schema({
   description: String,
   image: {
     type: String,
+    default:
+      "https://images.unsplash.com/photo-1734375181552-cfa83d404033?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     set: (v) =>
       v === ""
         ? "https://images.unsplash.com/photo-1734375181552-cfa83d404033?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -18,5 +19,6 @@ const listingSchema = new Schema({
   location: String,
   country: String,
 });
+
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
